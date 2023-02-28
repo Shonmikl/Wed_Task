e85cf9afd5274ec7312518444672f7b85468d7cf
shonmikl <mikhailegorov676@gmail.com>
2023-02-28 16:33:06 +0300

    Палиндром

---
+++ src/_2023_02_28/IE.java
@@ -0,0 +1,22 @@
+//package _2023_02_28;
+//
+//import java.util.*;
+//
+//public class IE {
+//    public static void main(String[] args) {
+////        ArrayList<String> set = new ArrayList<>();
+//        String s = "MADAM";
+//
+////        set.add("@@@");
+////        set.add("###");
+////        set.add("!!!");
+////        set.add("$$$");
+//
+//        //@@@ ### !!! $$$
+//        //Iterator<String> iterator = set.iterator();
+//        ListIterator<String> iterator = set.listIterator(set.size()-1);
+//        while (iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
+//    }
+//}
---
+++ src/_2023_02_28/LIE.java
@@ -0,0 +1,24 @@
+package _2023_02_28;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LIE {
+    //Метод, который генерит слова заглавными буквами,
+    //используя 5 букв
+    //Написать метод который добавляет в лист 1000 слов
+    //Написать метод который принимает Лист и проверяет каждое слово
+    //на палиндром
+    //Вывод на экран СЛОВО : Палиндром/НЕ Палиндром
+    public static void main(String[] args) {
+        String s = "MADAM";
+        List<Character> characters = new LinkedList<>();
+        for(char c : s.toCharArray()) {
+            characters.add(c);
+        }
+
+        //l1
+        //l2
+        boolean isPalindrome;
+    }
+}
