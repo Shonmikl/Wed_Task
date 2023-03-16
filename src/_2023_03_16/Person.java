22cac2c058d76fbe1f75635203c5d60e439ea700
shonmikl <mikhailegorov676@gmail.com>
2023-03-16 16:15:04 +0300

    Pom

--- src/_2023_03_16/Person.java
+++ src/_2023_03_16/Person.java
@@ -5,13 +5,38 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Comparator;
+import java.util.Objects;
+
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
-public class Person {
+public class Person implements Comparable<Person> {
     private String name;
     private String department;
-    private int age;
-    private double salary;
+    private Integer age;
+    private int salary;
+
+    @Override
+    public int compareTo(Person p2) {
+      // return this.salary == p2.salary ? this.age - p2.age : this.salary - p2.salary;
+      //  return this.department.compareTo(p2.department);
+        return this.salary == p2.salary ? this.department.compareTo(p2.department) : this.salary - p2.salary;
+    }
+    //obj1.compareTo(obj2)
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return salary == person.salary
+                && Objects.equals(department, person.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(department, salary);
+    }
 }
