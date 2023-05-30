2132f716e3bd2c85e0c9379bd5cd2f5774a0c221
shonmikl <mikhailegorov676@gmail.com>
2023-05-30 17:19:37 +0200

    abq

--- src/_2023_05_30/ABQ.java
+++ src/_2023_05_30/ABQ.java
@@ -10,31 +10,35 @@ public class ABQ {
 
         Thread producer = new Thread(() -> {
             String[] names = {"Alexander", "Sergey", "Konstantin", "Elina", "Al", "Yan", "Lu", "Anita", "Sonja"};
-            for (int i = 0; i < names.length && !Thread.interrupted(); i++) {
+            for (int i = 0; i < names.length; i++) {
                 try {
                     Thread.sleep(1500);
                     queue.put(names[i]);
-                    System.out.println("Producer: " + names[i] + " : " + "COUNT --> " + queue.size());
+                    System.out.println("Producer: " + names[i] + " : " + "COUNT --> " + queue.size()
+                            + " ** " + Thread.currentThread().getName());
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
         });
 
-        Thread consumer1 = new Thread(()-> {
+        Thread consumer1 = new Thread(() -> {
             StringBuilder sb = new StringBuilder();
             while (!Thread.interrupted()) {
-
                 try {
                     sb.setLength(0);
                     Thread.sleep(5500);
                     sb.append(queue.take());
-                    System.out.println("Consumer: " + sb.reverse() + " : " + "COUNT --> " + queue.size());
+                    System.out.println("Consumer: " + sb.reverse() + " : " + "COUNT --> " + queue.size()
+                            + " ** " + Thread.currentThread().getName());
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
         });
+
+        producer.setName("PRO1");
+        consumer1.setName("CON1");
 
         producer.start();
         consumer1.start();
