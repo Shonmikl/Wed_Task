6a67c6327ee2931b06cc2decba13b523ec854c6f
shonmikl <mikhailegorov676@gmail.com>
2023-05-30 13:42:27 +0200

    abq

---
+++ src/_2023_05_30/ABQ.java
@@ -0,0 +1,42 @@
+package _2023_05_30;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ABQ {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(4);
+
+        Thread producer = new Thread(() -> {
+            String[] names = {"Alexander", "Sergey", "Konstantin", "Elina", "Al", "Yan", "Lu", "Anita", "Sonja"};
+            for (int i = 0; i < names.length && !Thread.interrupted(); i++) {
+                try {
+                    Thread.sleep(1500);
+                    queue.put(names[i]);
+                    System.out.println("Producer: " + names[i] + " : " + "COUNT --> " + queue.size());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread consumer1 = new Thread(()-> {
+            StringBuilder sb = new StringBuilder();
+            while (!Thread.interrupted()) {
+
+                try {
+                    sb.setLength(0);
+                    Thread.sleep(5500);
+                    sb.append(queue.take());
+                    System.out.println("Consumer: " + sb.reverse() + " : " + "COUNT --> " + queue.size());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        producer.start();
+        consumer1.start();
+    }
+}
---
+++ src/_2023_05_30/WN.java
@@ -0,0 +1,12 @@
+package _2023_05_30;
+
+public class WN {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Market {
+    private int breadCount = 0;
+
+}
