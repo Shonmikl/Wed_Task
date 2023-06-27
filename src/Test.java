0730c4f56f52220da4adbc50e32799b9dea3ab7b
shonmikl <mikhailegorov676@gmail.com>
2023-06-27 13:01:30 +0200

    Annotation

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        String s = "mama";
+        String b = null;
+
+        if ("mama".equals(b)) {
+            System.out.println("!!!");
+        }
+    }
+}
